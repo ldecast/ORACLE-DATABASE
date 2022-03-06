d6a659e6db253d5ad013fffcc2fe998ee65caaa8
Luis Da Castellanos <3006240180101@ingenieria.usac.edu.gt>
2022-03-06 01:13:48 -0600

    Carga masiva v1

---
+++ CargaMasiva/RUNABLE.sh
@@ -0,0 +1,8 @@
+#userid -- ORACLE username/password@IP:PUERTO/SID
+#control -- control file name
+#log -- log file name
+#bad -- bad file name
+sqlldr userid=admin/ldecast@34.132.89.215:1521/ORCL18 control=LOADER.ctl
+echo " "
+echo -e "\e[96m  ENTER PARA CONTINUAR ... \e[0m"
+read
