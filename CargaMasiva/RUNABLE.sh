e6b2da194ad8de513d3cffeb1df3e4a6b79fae25
Luis Da Castellanos <3006240180101@ingenieria.usac.edu.gt>
2022-03-06 21:40:07 -0600

    Carga masiva correcta

--- CargaMasiva/RUNABLE.sh
+++ CargaMasiva/RUNABLE.sh
@@ -3,6 +3,6 @@
 #log -- log file name
 #bad -- bad file name
 sqlldr userid=admin/ldecast@34.132.89.215:1521/ORCL18 control=LOADER.ctl
-echo " "
-echo -e "\e[96m  ENTER PARA CONTINUAR ... \e[0m"
-read
+# echo " "
+# echo -e "\e[96m  ENTER PARA CONTINUAR ... \e[0m"
+# read
