b11211945159f0c3c397fe4d9bd26028b806f82f
Luis Da Castellanos <3006240180101@ingenieria.usac.edu.gt>
2022-03-12 20:43:45 -0600

    Terminado v1

--- DDL/Vistas.sql
+++ DDL/Vistas.sql
@@ -1,5 +1,17 @@
 CREATE OR REPLACE VIEW "PACIENTES-ATENDIDOS" AS 
-SELECT empleado_idempleado idEmpleado, COUNT(DISTINCT paciente_idPaciente) pacientes_atendidos
-FROM EVALUACION
-GROUP BY empleado_idEmpleado
+    SELECT empleado_idempleado idEmpleado, COUNT(DISTINCT paciente_idPaciente) pacientes_atendidos
+    FROM EVALUACION
+    GROUP BY empleado_idEmpleado
+WITH READ ONLY;
+
+CREATE OR REPLACE VIEW "TOTAL-PACIENTES-ATENDIDOS" AS 
+    SELECT SUM(pacientes_atendidos) total_pacientes_atendidos
+    FROM "PACIENTES-ATENDIDOS"
+WITH READ ONLY;
+
+CREATE OR REPLACE VIEW "PRIMERA-EVALUACION-PACIENTE" AS 
+    SELECT paciente_idPaciente idPaciente, MIN(fechaEvaluacion) primeraEvaluacion
+    FROM EVALUACION
+    GROUP BY paciente_idPaciente
+    ORDER BY MIN(fechaEvaluacion)
 WITH READ ONLY;
--- DML/Consultas.sql
+++ DML/Consultas.sql
@@ -63,13 +63,82 @@ ORDER BY PT.cantidad_tratamientos DESC;
 SELECT DIAGNOSTICO.tipoFumador,
     D.cantidad_asignaciones
 FROM(
-
-) D
+        SELECT DE.diagnostico_idDiagnostico, DE.rangoDiagnostico, COUNT(*) cantidad_asignaciones
+        FROM "DIAGNOSTICO-EVALUACION" DE
+        GROUP BY DE.diagnostico_idDiagnostico, DE.rangoDiagnostico
+        HAVING DE.rangoDiagnostico = 9
+    ) D
     INNER JOIN DIAGNOSTICO ON DIAGNOSTICO.idDiagnostico = D.diagnostico_idDiagnostico
 ORDER BY D.cantidad_asignaciones DESC;
 
-/* SELECT DE.paciente_idPaciente, COUNT(*) cantidad_asignaciones
-FROM "DIAGNOSTICO-EVALUACION" DE
---INNER JOIN DIAGNOSTICO D ON D.idDiagnostico = DE.diagnostico_idDiagnostico
-GROUP BY DE.diagnostico_idDiagnostico
-HAVING DE.rangoDiagnostico = 9 */
+----- 7 -----
+SELECT DISTINCT P.nombre,
+    P.apellido,
+    P.direccion
+FROM PACIENTE P
+INNER JOIN EVALUACION E ON E.paciente_idPaciente = P.idPaciente
+INNER JOIN "DIAGNOSTICO-EVALUACION" DE ON DE.evaluacion_idEvaluacion = E.idEvaluacion AND DE.rangoDiagnostico > 5
+ORDER BY P.nombre, P.apellido DESC;
+
+----- 8 -----
+SELECT E.nombre,
+    E.apellido,
+    E.fechaNacimiento,
+    PA.pacientes_atendidos
+FROM EMPLEADO E
+INNER JOIN "PACIENTES-ATENDIDOS" PA ON PA.idEmpleado = E.idEmpleado
+WHERE E.genero = 'F'
+AND E.direccion = '1475 Dryden Crossing'
+ORDER BY PA.pacientes_atendidos DESC;
+
+----- 9 -----
+SELECT E.idEmpleado,
+    E.nombre,
+    E.apellido,
+    ROUND((PA.pacientes_atendidos/total_pacientes)*100,4) porcentaje
+FROM (
+    SELECT empleado_idempleado idEmpleado, COUNT(DISTINCT paciente_idPaciente) pacientes_atendidos
+    FROM EVALUACION
+    WHERE EXTRACT(YEAR FROM fechaEvaluacion) >= 2017
+    GROUP BY empleado_idEmpleado
+) PA,
+(SELECT COUNT(*) total_pacientes FROM PACIENTE),
+(SELECT idEmpleado, nombre, apellido FROM EMPLEADO) E
+WHERE E.idEmpleado = PA.idEmpleado
+ORDER BY porcentaje DESC;
+
+----- 10 -----
+SELECT T.titulo,
+    ROUND((T.conteo/total_empleados)*100,4) porcentaje
+FROM (
+    SELECT PROFESION.titulo, COUNT(*) conteo
+    FROM EMPLEADO
+    INNER JOIN PROFESION ON PROFESION.idProfesion = EMPLEADO.profesion_idProfesion
+    GROUP BY PROFESION.titulo
+) T,
+(SELECT COUNT(*) total_empleados FROM EMPLEADO)
+ORDER BY porcentaje DESC;
+
+----- 11 -----
+SELECT Q.año, Q.mes, P.nombre, P.apellido
+FROM (
+    SELECT EXTRACT(YEAR FROM MIN(PEP.primeraEvaluacion)) año, EXTRACT(MONTH FROM MIN(PEP.primeraEvaluacion)) mes, PEP.idPaciente
+    FROM "PRIMERA-EVALUACION-PACIENTE" PEP,
+    (
+        SELECT TP.paciente_idPaciente, COUNT(*) tratamientos_aplicados
+        FROM "TRATAMIENTO-PACIENTE" TP
+        GROUP BY TP.paciente_idPaciente
+        ORDER BY tratamientos_aplicados DESC
+        FETCH FIRST 5 ROWS ONLY
+    ) PMAS,
+    (
+        SELECT TP.paciente_idPaciente, COUNT(*) tratamientos_aplicados
+        FROM "TRATAMIENTO-PACIENTE" TP
+        GROUP BY TP.paciente_idPaciente
+        ORDER BY tratamientos_aplicados ASC
+        FETCH FIRST 5 ROWS ONLY
+    ) PMENOS
+    WHERE (PMAS.paciente_idPaciente = PEP.idPaciente OR PMENOS.paciente_idPaciente = PEP.idPaciente)
+    GROUP BY PEP.idPaciente
+) Q
+INNER JOIN PACIENTE P ON P.idPaciente = Q.idPaciente;
