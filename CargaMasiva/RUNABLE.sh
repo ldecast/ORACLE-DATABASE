957ce8835d4459d4a51d28712b139d8cce93738a
Luis Da Castellanos <3006240180101@ingenieria.usac.edu.gt>
2022-03-16 09:55:38 -0600

    Entrega

--- CargaMasiva/RUNABLE.sh
+++ CargaMasiva/RUNABLE.sh
@@ -2,7 +2,7 @@
 #control -- control file name
 #log -- log file name
 #bad -- bad file name
-sqlldr userid=admin/ldecast@34.132.89.215:1521/ORCL18 control=LOADER.ctl
+sqlldr userid=admin/ldecast@34.132.89.215:1521/ORCL18 control=[BD1]ArchivoControl.ctl
 # echo " "
 # echo -e "\e[96m  ENTER PARA CONTINUAR ... \e[0m"
 # read
